cc7f3022098722bff693520282248b22ec2e7807
65ra8e <willardth77@gmail.com>
2025-05-11 12:14:23 -0700

    eighth commit

---
+++ 6.variables.tf
@@ -0,0 +1,80 @@
+variable "project" {
+  type = string
+    default = "seahawks25"
+}
+ 
+
+variable "region" {
+  type = string
+  default = "us-west1"
+  
+}
+
+
+variable "region2" {
+  type = string
+  default = "asia-east1"
+  
+}
+
+variable "region3" {
+  type = string
+  default = "us-central1"
+  
+}
+
+variable "region4" {
+  type = string
+  default = "southamerica-east1"
+  
+}
+
+variable "ip_cidr_range" {
+  type = string
+  default = "10.206.77.0/24"
+  
+}
+
+variable "ip_cidr_range2" {
+  type = string
+  default = "10.206.97.0/24"
+  
+}
+
+variable "ip_cidr_range3" {
+  type = string
+  default = "10.30.77.0/24"
+  
+}
+
+variable "ip_cidr_range4"{
+  type = string
+  default = "10.30.97.0/24"
+  
+}
+
+variable "google_compute_subnetwork" {
+  type = string
+  default = "imfromseattle"
+  
+}
+
+  
+variable "google_compute_subnetwork2" {
+  type = string
+  default = "tokyo"
+  
+}
+
+variable "google_compute_subnetwork3" {
+  type = string
+  default = "iowa"
+}
+
+variable "google_compute_subnetwork4" {
+  type = string
+  default = "southamerica"
+}
+ 
+
+  
